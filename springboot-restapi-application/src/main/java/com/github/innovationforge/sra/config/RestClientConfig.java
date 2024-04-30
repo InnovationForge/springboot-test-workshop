10f7f576ed46e232be77c22e2e4ee9b990b11fac
Niranjan Rath <niranjan.rath@gmail.com>
2024-04-30 23:25:42 +0200

    this code sets up a RestClient with a connection pool, allowing for efficient reuse of connections when making multiple HTTP requests.

--- springboot-restapi-application/src/main/java/com/github/innovationforge/sra/config/RestClientConfig.java
+++ springboot-restapi-application/src/main/java/com/github/innovationforge/sra/config/RestClientConfig.java
@@ -1,7 +1,12 @@
 package com.github.innovationforge.sra.config;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -9,6 +14,17 @@ public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
-        return RestClient.create();
+        PoolingHttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+                .setMaxConnTotal(100)
+                .setMaxConnPerRoute(20)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .build();
+
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
+        return RestClient.builder().requestFactory(requestFactory).build();
     }
 }
